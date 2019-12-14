6fa1e54c37c1d13de0ced448b0e928e9c28cd3ff
THEBIG0 <owendcgunter@gmail.com>
2019-12-14 17:39:34 +1030

    Platform Generator and Destroyer
    Infinite loop of platforms generating a set distance and destroying at set points both of which are dictated by the camera position

---
+++ Assets/Scripts/PlatformGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformGenerator : MonoBehaviour
+{
+    public GameObject thePlatform;
+    public Transform generationPoint;
+    public float distanceBetween;
+
+    private float platformWidth;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //gets the platform width from the boxcollider attached to platform
+        platformWidth = thePlatform.GetComponent<BoxCollider2D>().size.x;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(transform.position.x < generationPoint.position.x)
+        {
+            transform.position = new Vector3(transform.position.x + platformWidth + distanceBetween, transform.position.y, transform.position.z);
+
+            Instantiate(thePlatform, transform.position, transform.rotation);
+        }
+    }
+}
